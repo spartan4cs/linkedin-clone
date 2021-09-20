e102e61863e8a0edc160c15cd6aafa3736b48b67
Akash Kumbhare <aakashkumbhare@gmail.com>
2021-09-20 18:08:52 +0000

    LinkedIn | Building the Header Options

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+npx create-react-app --template redux
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app), using the [Redux](https://redux.js.org/) and [Redux Toolkit](https://redux-toolkit.js.org/) template.
 
 ## Available Scripts
--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,38 @@
 import React from "react";
-import SearchIcon from "@mui/icons-material/Search";
 import "./Header.css";
-import { Search } from "@mui/icons-material";
+import {
+  BusinessCenter,
+  Chat,
+  Home,
+  Notifications,
+  Search,
+  SupervisorAccount,
+} from "@mui/icons-material";
+import HeaderOption from "./HeaderOption";
 const Header = () => {
   return (
     <div className="header">
       <div className="header__left">
-        <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="" />
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+          alt=""
+        />
         <div className="header__search">
           <Search />
-          <input type="text" />
+          <input type="text" placeholder="Search" />
         </div>
       </div>
-      <div className="header__right"></div>
+      <div className="header__right">
+        <HeaderOption title="Home" Icon={Home} />
+        <HeaderOption title="My Network" Icon={SupervisorAccount} />
+        <HeaderOption title="Jobs" Icon={BusinessCenter} />
+        <HeaderOption title="Messaging" Icon={Chat} />
+        <HeaderOption title="Notification" Icon={Notifications} />
+        <HeaderOption
+          title="Me"
+          avatar="https://pbs.twimg.com/profile_images/1393954096776761344/XXUnwfUF_400x400.jpg"
+        />
+      </div>
     </div>
   );
 };
